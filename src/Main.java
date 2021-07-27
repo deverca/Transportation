0661cbacbdf18f633bf4fc591bf571e7b4f9fa62
deverca <deverca@oocl.com>
2021-07-27 17:42:48 +0800

    solve transportation problem

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,9 @@ public class Main {
         Bus bus = new Bus();
         Train train = new Train();
         double budget = 500;
-        System.out.println("With the budget of " + budget + ",you can choose this/these vehicles to get to Beijing from Wuhan");
+        System.out.println("From Wuhan to Beijing you can choose from 4 means of transportation, including driving a car, taking bus, train, or plane.\n " +
+                "Driving costs 500 yuan, bus 300 yuan, train 400 yuan, and plane 1000 yuan.\n" +
+                " With the budget of 500 yuan, which vehicles Xiaoming can choose to get there?\nAnswers:");
         driving.checkIfBudgetFits(budget);
         plane.checkIfBudgetFits(budget);
         bus.checkIfBudgetFits(budget);
